38a367e9d9add0037567963e05431cc797a019ae
Muedgar <edgarmutangana@gmail.com>
2022-04-28 00:03:31 +0200

    last commit today

--- public/viewSinglePost.js
+++ public/viewSinglePost.js
@@ -1,3 +1,7 @@
+function hideSinglePost() {
+    document.getElementById("viewSinglePost").style.display = "display";
+}
+hideSinglePost();
 document.getElementById("viewSinglePostClose").addEventListener('click',()=> {
     document.getElementById("viewSinglePost").style.display = "none";
 });
